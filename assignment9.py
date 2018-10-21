8559fea1fa7c0f33f89d45e25970b1da910a1a8d
Ashutosh Kumar <ashutoshk167@gmail.com>
2018-10-22 01:11:01 +0530

    Added Finite Difference Method

--- assignment9.py
+++ assignment9.py
@@ -9,8 +9,10 @@
 
 import math
 import copy
+import random
 import numpy as np
 import matplotlib.pyplot as plt
+from assignment4 import gaussSeidel
 
 np.set_printoptions(formatter={'float': lambda x: "{0:0.6f}".format(x)})
 degree_sign = u'\N{DEGREE SIGN}'
@@ -48,9 +50,6 @@ def runge_kutta(T0,x0,z0,h = 0.5):
 		z0 = z1
 	return sol
 
-def difference():
-	return 0
-
 def shooting(x1,x2,T1,T2):
 	z0 = 10
 	shot_1 = runge_kutta(T1,x1,z0)
@@ -62,6 +61,49 @@ def shooting(x1,x2,T1,T2):
 	hit = runge_kutta(T1,x1,z0)
 
 	return shot_1,shot_2,hit
+
+def difference(x1,x2,T1,T2, n = 10):
+	h_prime = 0.01
+	T_a = 20
+
+	h = x2//n
+
+	A = np.zeros((n-1,n-1))
+	b = np.zeros(n-1)
+	T = np.zeros(n+1)
+
+	for i in range(n-1):
+		A[i][i] = h**2 * h_prime + 2
+		b[i] = h**2 * h_prime * T_a
+
+		if (i != 0):
+			A[i][i-1] = -1
+
+		if (i != n-2):
+			A[i][i+1] = -1
+
+	b[0] += T1
+	b[n-2] += T2
+	#print(A,"\n\n",b)
+	guess = random.sample(range(1,100),9)
+	guess = np.array(guess)
+	#print(guess)
+
+	(sol,r) = gaussSeidel(A,b,guess)
+	T[0] = T1
+	T[n] = T2
+
+	for i in range(n-1):
+		T[i+1] = sol[i]
+
+	#print(T)
+	i = 0
+	x = []
+	while (i <= x2):
+		x.append(i)
+		i += h
+
+	return T,x
 
 if __name__ == "__main__" :
 
@@ -89,12 +131,12 @@ if __name__ == "__main__" :
 	act_T = []
 	t = 0
 	h = 0.02
-	x = []
+	act_x = []
 
 	lim = 10
 	while (t <= 10):
 		act_T.append(act_f(t))
-		x.append(t)
+		act_x.append(t)
 		t += h
 
 	#Plot Solution of Shooting Method
@@ -102,9 +144,22 @@ if __name__ == "__main__" :
 	plt.plot(shot_1_x, shot_1_T, label = "Shot 1")
 	plt.plot(shot_2_x, shot_2_T, label = "Shot 2")
 	plt.plot(hit_x, hit_T, label = "Hit")
-	plt.plot(x, act_T,'--', label = "Analytical Solution")
+	plt.plot(act_x, act_T,'--', label = "Analytical Solution")
 	plt.xlabel("Length (m)")
 	plt.ylabel("Temperature ("+ degree_sign +"C)")
 	plt.title("Shooting Method")
 	plt.legend()
+	plt.show()
+
+	(diff_T, x) = difference(0,10,40,200)
+	#print (diff_T)
+
+	plt.grid(True)
+	plt.plot(x,diff_T, label = "Diff. Method")
+	plt.plot(act_x, act_T,'--', label = "Analytical Solution")
+	plt.xlabel("Length (m)")
+	plt.ylabel("Temperature ("+ degree_sign +"C)")
+	plt.title("Finite Difference Method")
+	plt.legend()
+	plt.show()
 	plt.show()
